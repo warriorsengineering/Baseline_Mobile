b436519b7f5d9f4b5e1b960ffc55bf071d9aba11
Darrell Smith Jr <dsmith@warriors.com>
2024-05-20 09:35:21 -0700

    added game preview card feature

--- src/screens/Schedule.tsx
+++ src/screens/Schedule.tsx
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { SafeAreaView, ScrollView, StatusBar, useColorScheme, View } from 'react-native';
-import { TabSwitcher } from '../components/Components';
+import { GameCard, TabSwitcher } from '../components/Components';
 
 const Schedule = () => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -28,14 +28,34 @@ const Schedule = () => {
             />
           </View>
 
-          {/* Schedule */}
-          <View className="mt-3" key={selectedTab}>
+          {/* Game Preview Card */}
+          <View className="mt-3 mb-3" key={selectedTab}>
             {selectedTab === 'Warriors' && (
               <>
+              <GameCard
+                  broadcast={'NBC Sports'}
+                  gameLocation={'vs'}
+                  gameTime={'Sun, Feb 25 at 7:00 PM'}
+                  opponentInitial={'DEN'}
+                  opponentLogo={require('../assets/images/nuggets-logo.png')}
+                  opponentRecord={'45-22'}
+                  warriorsInitial={'GSW'}
+                  warriorsLogo={require('../assets/images/warriors-logo.png')}
+                  warriorsRecord={'65-0'} valkyriesInitial={''} valkyriesLogo={0} valkyriesRecord={''}              />
               </>
             )}
             {selectedTab === 'Valkyries' && (
               <>
+            <GameCard
+                  gameTime={'Mon, Jun 24 at 4:30 PM'}
+                  gameLocation={'@'}
+                  valkyriesLogo={require('../assets/images/valkyries-logo.png')}
+                  valkyriesInitial={'GSV'}
+                  opponentInitial={'LVA'}
+                  valkyriesRecord={'0-0'}
+                  opponentRecord={'0-0'}
+                  opponentLogo={require('../assets/images/aces-logo.png')}
+                  broadcast={'NBC Sports'} warriorsInitial={''} warriorsLogo={0} warriorsRecord={''}            />
               </>
             )}
           </View>
